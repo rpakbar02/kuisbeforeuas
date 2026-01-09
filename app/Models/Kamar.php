fd91126e4286459e690258a2a9becb8261ce6ecf
raihanpakbar <raihanp.akbar@gmail.com>
2026-01-09 11:44:07 +0700

    0.7 perbaiki

--- app/Models/Kamar.php
+++ app/Models/Kamar.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Kamar extends Model
 {
     protected $table = 'kamar';
+    
+    // TODO: Definisikan kolom yang dapat diisi (mass assignment)
     protected $fillable = ['nomor_kamar', 'tipe', 'harga_bulanan', 'fasilitas', 'status'];
     
     public function kontrakSewa()
@@ -18,4 +20,4 @@ class Kamar extends Model
     {
         return $this->hasManyThrough(Penyewa::class, KontrakSewa::class, 'kamar_id', 'id', 'id', 'penyewa_id');
     }
-}
+}   
--- app/Models/Penyewa.php
+++ app/Models/Penyewa.php
@@ -12,11 +12,7 @@ class Penyewa extends Model
     protected $fillable = ['nama_lengkap', 'nomor_telepon', 'nomor_ktp', 'alamat_asal', 'pekerjaan'];
     
     // TODO: Definisikan relasi ke tabel lain
-<<<<<<< HEAD
-    public function pemesanan()
-    {
-        return $this->hasMany(Pemesanan::class, 'penyewa_id');
-=======
+
     public function kontrakSewa()
     {
         return $this->hasMany(KontrakSewa::class, 'penyewa_id');
@@ -24,6 +20,5 @@ class Penyewa extends Model
     public function kamar()
     {
         return $this->hasManyThrough(Kamar::class, KontrakSewa::class, 'penyewa_id', 'id', 'id', 'kamar_id');
->>>>>>> def159aba11868f4e34da06103096bd0a29f6895
     }
 }
