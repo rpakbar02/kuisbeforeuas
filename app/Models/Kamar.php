5cf60a59074f121d51f3cd967c8a7503b6ff7f94
rf22049 <105222049@student.universitaspertamina.ac.id>
2026-01-09 11:43:14 +0700

    commit

--- app/Models/Kamar.php
+++ app/Models/Kamar.php
@@ -7,26 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Kamar extends Model
 {
     protected $table = 'kamar';
-    
-<<<<<<< HEAD
-    public $fillable = ['nomor_kamar', 'tipe', 'harga_bulanan', 'fasilitas', 'status'];
-
-    public function kontrakSewa() {
-        return $this->hasMany(KontrakSewa::class);
-    }
-
-
-    public $fillable = ['nama_lengkap', 'nomor_telepon', 'nomor_ktp', 'alamat_asal', 'pekerjaan'];
-
-    public function kontrakSewa() {
-        return $this->hasMany(KontrakSewa::class);
-    }
-}
-=======
-    // TODO: Definisikan kolom yang dapat diisi (mass assignment)
     protected $fillable = ['nomor_kamar', 'tipe', 'harga_bulanan', 'fasilitas', 'status'];
     
-    // TODO: Definisikan relasi ke tabel lain
     public function kontrakSewa()
     {
         return $this->hasMany(KontrakSewa::class, 'kamar_id');
@@ -36,5 +18,4 @@ class Kamar extends Model
     {
         return $this->hasManyThrough(Penyewa::class, KontrakSewa::class, 'kamar_id', 'id', 'id', 'penyewa_id');
     }
-}   
->>>>>>> def159aba11868f4e34da06103096bd0a29f6895
+}
